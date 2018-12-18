4db4d4f2c8ea8f13f59b85302e9dc8cba3727a51
cyxdy930719 <cyxdy930719@163.com>
2018-12-18 08:22:55 +0800

    ajaxTest

--- src/com/neuedu/controller/LoginServlet.java
+++ src/com/neuedu/controller/LoginServlet.java
@@ -1,7 +1,14 @@
 package com.neuedu.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class LoginServlet extends HttpServlet {
-
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(1);
+    }
 }
